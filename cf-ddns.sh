676f5bb307ae40dd36e9ecc00aae3e9d7d360041
kevinkingdomviva <33989222+kevinkingdomviva@users.noreply.github.com>
2019-09-26 01:36:34 +0800

    Add files via upload

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -3,11 +3,11 @@
 # CHANGE THESE
 auth_email="kevinkingdomviva@gmail.com"
 auth_key="f3f3eedae29830602af399880ff9f8860d111" # found in cloudflare account settings
-zone_name="doudoulong.club"
-record_name="ddns.doudoulong.club"
+zone_name="doudoulong.services"
+record_name="16hk.doudoulong.services"
 
 # MAYBE CHANGE THESE
-ip=$(curl -s http://ipv4.icanhazip.com)
+ip=$(curl -s http://ipv4.ip.sb)
 ip_file="ip.txt"
 id_file="cloudflare.ids"
 log_file="cloudflare.log"
@@ -30,15 +30,15 @@ if [ -f $ip_file ]; then
     fi
 fi
 
-if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
-    zone_identifier=$(head -1 $id_file)
-    record_identifier=$(tail -1 $id_file)
-else
+#if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
+#    zone_identifier=$(head -1 $id_file)
+#    record_identifier=$(tail -1 $id_file)
+#else
     zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
     record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
-    echo "$zone_identifier" > $id_file
-    echo "$record_identifier" >> $id_file
-fi
+#    echo "$zone_identifier" > $id_file
+#    echo "$record_identifier" >> $id_file
+#fi
 
 update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" -H "Content-Type: application/json" --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\"}")
 
